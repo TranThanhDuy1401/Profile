acb1d21835e0ae409ce5d209e7f3e713a5549c74
Tran Thanh Duy <tranthanhduyst246@gmail.com>
2026-04-20 19:33:38 +0700

    Fix images and change zooming effect

--- thuvienanh.js
+++ thuvienanh.js
@@ -1,10 +1,10 @@
-const images = document.querySelectorAll(".thuvienanh");
-const modal = document.getElementById("imageModal");
-const modalImg = document.getElementById("modalImg");
-const closeBtn = document.querySelector(".close");
+const images = document.querySelectorAll(".thuvienanh"); // biến images = lấy toàn bộ ảnh có class .thuvienanh
+const modal = document.getElementById("imageModal");  // biến modal = div của khung chứa ảnh chế độ full screen
+const modalImg = document.getElementById("modalImg"); // biến modalImg = hình ảnh được hiện lên ở chế độ full screen
+const closeBtn = document.querySelector(".close"); // nút X đóng ảnh
 
 // Click vào ảnh
-images.forEach(img => {
+images.forEach(img => { 
     img.addEventListener("click", () => {
         modal.classList.add("show");
         modalImg.src = img.src;
